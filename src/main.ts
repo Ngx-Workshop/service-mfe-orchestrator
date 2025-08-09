8e70ee9562fe53abd55cce1f21fd284f2a82b281
Wesley DuSell <dusellfam@gmail.com>
2025-08-08 20:58:02 -0400

    Update Auth Client

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true })
   );
 
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
 }
 bootstrap();
